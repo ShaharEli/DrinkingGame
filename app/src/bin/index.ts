920755d1ea1c3634ba9c401469cb98648e36d33b
Shahar Eliyahu <shahar.e3@gmail.com>
2021-08-06 19:34:30 +0300

    feat(all): lint on commit msg added

--- app/src/bin/index.ts
+++ app/src/bin/index.ts
@@ -8,7 +8,7 @@ export const apiHost =
   Platform.OS === 'android' && isSimulator()
     ? 'http://10.0.2.2:8080'
     : 'http://localhost:8080';
-export const apiHostWithVersion = apiHost + '/api/v1';
+export const apiHostWithVersion = `${apiHost}/api/v1`;
 
 async function isSimulator() {
   return await DeviceInfo.isEmulator();
