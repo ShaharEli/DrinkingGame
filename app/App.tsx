b8f59bdf2d5e3638a87a2943d52039517c22b26b
Shahar Eliyahu <shahar.e3@gmail.com>
2021-08-14 18:39:50 +0300

    feat(redux): persist added

--- app/App.tsx
+++ app/App.tsx
@@ -12,18 +12,21 @@ import React from 'react';
 import {LogBox} from 'react-native';
 import {LOGS_TO_IGNORE} from './src/utils';
 import {Provider} from 'react-redux';
-import {store} from './src/redux/store';
+import {persistor, store} from './src/redux/store';
 import RoutesContainer from './src/routes/RoutesContainer';
 import ErrorBoundary from './src/screens/ErrorBoundry';
+import {PersistGate} from 'redux-persist/integration/react';
 
 LogBox.ignoreLogs(LOGS_TO_IGNORE);
 
 const App = (): JSX.Element => {
   return (
     <Provider store={store}>
-      <ErrorBoundary>
-        <RoutesContainer />
-      </ErrorBoundary>
+      <PersistGate persistor={persistor} loading={null}>
+        <ErrorBoundary>
+          <RoutesContainer />
+        </ErrorBoundary>
+      </PersistGate>
     </Provider>
   );
 };
--- app/src/redux/store.ts
+++ app/src/redux/store.ts
@@ -1,14 +1,41 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import styleReducer from './slices/stylesSlice';
 import userReducer from './slices/userSlice';
 import gameReducer from './slices/gameSlice';
-export const store = configureStore({
-  reducer: {
-    styles: styleReducer,
-    user: userReducer,
-    game: gameReducer,
-  },
+import {persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import persistStore from 'redux-persist/es/persistStore';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist/es/constants';
+
+const reducers = combineReducers({
+  styles: styleReducer,
+  user: userReducer,
+  game: gameReducer,
 });
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
